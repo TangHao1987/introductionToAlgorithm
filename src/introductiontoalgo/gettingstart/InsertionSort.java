92d0c1b6a3706a5cce6dcbed419d9b159edbb2ab
TangHao1987 <tanghaol1987@gmail.com>
2013-10-22 00:36:57 +0800

    added first algorithm program InsertionSort

---
+++ src/introductiontoalgo/gettingstart/InsertionSort.java
@@ -0,0 +1,35 @@
+package introductiontoalgo.gettingstart;
+
+public class InsertionSort {
+	public static void main(String[] args) {
+		// define your input in this array
+		int[] input = { 1, 10, 100, 56, 27, 42, 6 };
+		// output array
+		int[] output = input;
+
+		System.out.print("input int String is: [ ");
+		for (int i : input) {
+			System.out.print(i + " ");
+		}
+		System.out.println("]");
+
+		// algorithm start
+		if (input.length > 1) {
+			for (int iterator = 1; iterator < input.length; iterator++) {
+				int key = output[iterator];
+				int innerIterator = iterator - 1;
+				while (innerIterator >= 0 && output[innerIterator] > key) {
+					output[innerIterator + 1] = output[innerIterator];
+					innerIterator--;
+				}
+				output[innerIterator+1] = key;
+			}
+		}
+
+		System.out.print("output int String is: [ ");
+		for (int i : output) {
+			System.out.print(i + " ");
+		}
+		System.out.println("]");
+	}
+}
