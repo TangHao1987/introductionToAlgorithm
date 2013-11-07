16a2b90fa91880245cf76fa12d45ef3922f9906c
TangHao1987 <tanghaol1987@gmail.com>
2013-11-07 23:14:27 +0800

    commit new execrise answer

---
+++ src/introductiontoalgo/divideAndConquer/maximumSubarray/Exercise_4_1_2_MaximumSubarrayWithBruteFroce.java
@@ -0,0 +1,48 @@
+package introductiontoalgo.divideAndConquer.maximumSubarray;
+
+import introductiontoalgo.common.CommonLogics;
+import introductiontoalgo.common.MaxSubarrayOutputHolder;
+
+/**
+ * 
+ * This the the most tranditional brute force maximum subarray implementation
+ * BIG-O of this logic is n^2
+ * 
+ * @author Tang Hao
+ *
+ */
+public class Exercise_4_1_2_MaximumSubarrayWithBruteFroce extends CommonLogics{
+	public static void main(String [] args){
+		int [] inputArray = {-12 , -42 , 24, 23 , 43 , 64 ,3 ,64 ,75, -24};
+		printIntArray(inputArray, true);
+		
+		String outputHolder = bruteForceMaximumSubArray(inputArray);
+		
+		System.out.println(outputHolder);
+	}
+
+	public static String bruteForceMaximumSubArray(int[] inputArray) {
+		int arraySize = inputArray.length*(inputArray.length + 1)/2;
+		int [] lengthArray = new int [arraySize];
+		int lengthArrayIterator = 0;
+		for(int i = 0; i < inputArray.length ; i++){
+			int sum = 0;
+			for(int j = i ; j< inputArray.length; j++ ){
+				sum += inputArray[j];
+				lengthArray[lengthArrayIterator] = sum;  
+				lengthArrayIterator++;
+			}
+		}
+		
+		int maxSum = -1000000;
+		int maxSumIndex = 0;
+		for(int i = 0; i< lengthArray.length ; i++){
+			if (lengthArray[i]> maxSum){
+				maxSum = lengthArray[i];
+				maxSumIndex = i;
+			}
+		}
+		
+		return new Integer(maxSum).toString();
+	}
+}
--- src/introductiontoalgo/divideAndConquer/maximumSubarray/Exercise_4_1_5_MaximumSubarrayWithCustomLogic.java
+++ src/introductiontoalgo/divideAndConquer/maximumSubarray/Exercise_4_1_5_MaximumSubarrayWithCustomLogic.java
@@ -5,32 +5,31 @@ import introductiontoalgo.common.MaxSubarrayOutputHolder;
 
 /**
  * 
- * Very tranditional way to solve the Maximum Subarray problem.
- * to find the maximum subarray by iterating through all the elements by two loops
+ * This is the algorithm described in 4.1.5
  * BIG-O of this logic is n^2
  * 
  * @author Tang Hao
  *
  */
-public class Exercise_4_1_2_MaximumSubarrayWithBruteForce extends CommonLogics{
+public class Exercise_4_1_5_MaximumSubarrayWithCustomLogic extends CommonLogics{
 	public static void main(String [] args){
 		int [] inputArray = {12 , 42 , 24, 23 , -43 , 64 ,3 ,64 ,75, 24};
 		printIntArray(inputArray, true);
 		
-		MaxSubarrayOutputHolder outputHolder = bruteForceMaximumSubArray(inputArray);
+		MaxSubarrayOutputHolder outputHolder = customAlgoMaximumSubArray(inputArray);
 		
 		System.out.println(outputHolder);
 	}
 
-	public static MaxSubarrayOutputHolder bruteForceMaximumSubArray(int[] inputArray) {
+	public static MaxSubarrayOutputHolder customAlgoMaximumSubArray(int[] inputArray) {
 		int startIndex = 0;
 		int endIndex = 0;
 		int maxSum = -10000;
-		
 		for(int i = 0; i < inputArray.length ; i++){
 			int loopSum = 0;
 			int loopSumMax = -10000;
 			int loopMaxEndIndex = 0;
+			
 			for(int j = i ; j< inputArray.length; j++ ){
 				loopSum += inputArray[j];
 				if(loopSum > loopSumMax){
